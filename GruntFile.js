0503f21d5bd93789f8df7d972954012ba4c5e00f
Rus@Progtech <punyarit.kla.progtech@gmail.com>
2024-05-02 12:41:31 +0700

    second commit

--- GruntFile.js
+++ GruntFile.js
@@ -4,13 +4,24 @@ module.exports = function (grunt) {
     uglify: {
       build: {
         files: [{
-          src: 'script/jquery-3.7.1.js',
+          src: 'scripts/jquery-3.7.1.js',
           dest: 'build/jquery-3.7.1.min.js'
         }]
+      }
+    },
+    concat: {
+      js: {
+        src: ['scripts/*.js'],
+        dest: 'build/concat-scripts.js'
+      },
+      css: {
+        src: ['styles/*.css'],
+        dest: 'build/concat-styles.css'
       }
     }
   });
   //Load plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   //Register Tasks
 }
